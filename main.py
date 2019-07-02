4469aea6b54b52beea2e1ad9e8bf7143ab0fbf3b
HLAM <hemilioaraujo@gmail.com>
2019-07-02 10:58:29 -0300

    adicionado arquivos faltantes

---
+++ main.py
@@ -0,0 +1,9 @@
+from CursoEmVideo import CursoEmVideo
+import time
+
+a = CursoEmVideo()
+
+#BASTA REMOVER O "#" DA LINHA DE BAIXO E UTILIZAR A FUNÇÃO QUE DESEJAR
+#a.cores_no_terminal()
+
+time.sleep(10)
